4bdcb2f3a5943a1b1eab2a18f2a8d4df6ae12c3b
VictorAnnell <30280491+VictorAnnell@users.noreply.github.com>
2023-11-15 18:59:18 +0100

    Create Compute class

--- app/main.py
+++ app/main.py
@@ -12,19 +12,29 @@ class Payload:
     module: str
     data: str
 
+class Compute:
+    @classmethod
+    async def create(cls):
+        self = cls()
+        self.modules = []
+        self.nats_url = os.getenv("BROKER_URL", "nats://localhost:4222")
+        self.nc = await nats.connect(self.nats_url)
+        return self
+
+    async def receive(self):
+        sub = await self.nc.subscribe("updates")
+        while(True):
+            async for message in sub.messages:
+                try:
+                    payload = Payload(**json.loads(message.data))
+                    print(f"received valid JSON payload: {payload.id=} {payload.module=} {payload.data=}")
+                except json.decoder.JSONDecodeError:
+                    print(f"received invalid JSON payload: {message.data=}")
+
 
 async def main():
-    nats_url = os.getenv("BROKER_URL", "nats://localhost:4222")
-    nc = await nats.connect(nats_url)
-
-    sub = await nc.subscribe("updates", max_msgs=2)
-    while(True):
-        async for message in sub.messages:
-            try:
-                payload = Payload(**json.loads(message.data))
-                print(f"received valid JSON payload: {payload.id=} {payload.module=} {payload.data=}")
-            except json.decoder.JSONDecodeError:
-                print(f"received invalid JSON payload: {message.data=}")
+    compute = await Compute.create()
+    await compute.receive()
 
 
 

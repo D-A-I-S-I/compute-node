cf0e93581f6b30a268b4fe4aaf9c4840bf6c51f3
VictorAnnell <30280491+VictorAnnell@users.noreply.github.com>
2023-12-20 16:23:23 +0100

    Improve module loading and usage from main

--- app/main.py
+++ app/main.py
@@ -19,39 +19,50 @@ class Compute:
     @classmethod
     async def create(cls):
         self = cls()
-        self.modules = []
+        self.modules = {}
         self.nats_url = os.getenv("BROKER_URL", "nats://localhost:4222")
         self.nc = await nats.connect(self.nats_url)
+        print(f"Connected to {self.nats_url}")
+        self.load_modules()
         return self
+
+    def load_modules(self):
+        for module in modules.__all__:
+            c = getattr(modules, module)()
+            self.modules[c.module_name] = c
+            print(f"Loaded module: {module}")
 
     def transform(self):
         command = "python3 app/json2pcap.py -i ./sample.json -o ./converted.pcap"
         formatted_command = shlex.split(command)
         subprocess.run(formatted_command)
-        
+
     async def receive(self):
+        print("Receiving")
         sub = await self.nc.subscribe("updates")
         while (True):
             async for message in sub.messages:
                 try:
                     payload = Payload(**json.loads(message.data))
                     print(
-                        f"received valid JSON payload: {payload.id=} {payload.module=} {payload.data=}")
-                    
-                    if (payload.module=="network_traffic"):
-                        for point in payload.data:
-                            with open("sample.json", "w") as outfile:
-                                json.dump(point, outfile)
-                        self.transform()
-
-                        
+                        f"received valid JSON payload: {payload.id=} {payload.module=} {payload.data=}"
+                    )
+                    try:
+                        self.modules[payload.module].write_to_buffer(payload.data)
+                    except KeyError:
+                        print(f"Module {payload.module} not found")
                 except json.decoder.JSONDecodeError:
                     print(f"received invalid JSON payload: {message.data=}")
 
+    async def run(self):
+        print("Running")
+        asyncio.gather(*[module.run() for module in self.modules.values()])
+        await self.receive()
+        await self.nc.drain()
 
 async def main():
     compute = await Compute.create()
-    await compute.receive()
+    await compute.run()
 
 
 if __name__ == "__main__":
